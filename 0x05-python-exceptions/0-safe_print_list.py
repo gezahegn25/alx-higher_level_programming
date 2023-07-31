65d3a9f0666a934c80715a052bce8da2f7dd92d5
gezahegn25 <gezahegns988@gmail.com>
2023-07-31 02:33:07 -0700

    print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elememts of a list.
+
+    Args:
+        my_list (list): lists to print elements from.
+        x (int): number of elements of my_list to print.
+
+    Returns:
+        number of elements printed.
+    """
+
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
+
