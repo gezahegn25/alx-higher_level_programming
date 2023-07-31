82ddd1e54678f45ebef5236c0a7fbac3a235da49
gezahegn25 <gezahegns988@gmail.com>
2023-07-31 04:24:17 -0700

    safe_function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import sys
 
 
@@ -11,3 +10,4 @@ def safe_function(fct, *args):
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+
