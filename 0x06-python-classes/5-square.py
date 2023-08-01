f4c8aecb0888b4c44749c0c2eda8d44f120e3f64
gezahegn25 <gezahegns988@gmail.com>
2023-08-01 12:51:13 -0700

    5-square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""Define class Square."""
+
 
 class Square:
+    """Represent square."""
 
     def __init__(self, size):
+        """Initialize new square.
+        Args:
+            size (int): The size of new square.
+        """
         self.size = size
 
+    @property
     def size(self):
+        """Get/set the current size of square."""
         return (self.__size)
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -16,9 +26,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
